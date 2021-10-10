984d0fb8a01acf192d7e93369c3f3f03c4d6fd69
Gabriel Magalhães <31325728+gbrmgls@users.noreply.github.com>
2021-10-10 18:00:13 -0300

    Update route_example.js

--- routes/route_example.js
+++ routes/route_example.js
@@ -6,8 +6,20 @@ router.get("/", (req, res, next) => {
     res.status(200).send("Hello!");
 });
 
-router.get("/:id", (req, res, next) => {
+router.get("/db_test", (req, res, next) => {
+    db.raw("SELECT * FROM TableName")
+        .then((data) => {
+            res.send(data);
+        }).catch(err => {
+            console.log(err)
+            res.send(err);
+        })
+});
+
+router.get("/param_test/:id", (req, res, next) => {
     res.status(200).send(`Hello! Parameter: ${req.params.id}`);
 });
+
+
 
 module.exports = router;
